fa6fdd825f9aa9ec6cd2b57cfcffeff7cfb088c2
Kyle Huang <kylehuang06@gmail.com>
2023-11-13 11:46:10 -0800

    Low Pass Filter

---
+++ Assets/Scripts/Vectors/SmoothedTransformVelocityReference.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SmoothedTransformVelocityReference : IVelocityReference
+{
+    private Vector3 lastPos;
+    private Vector3 velocity;
+    private Vector3 resultant;
+    public float alpha;
+    private float oneMinusAlpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = transform.position;
+        oneMinusAlpha = 1 - alpha;
+        resultant = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 posDiff = transform.position - lastPos;
+        velocity = posDiff / Time.fixedDeltaTime;
+
+        resultant = alpha * resultant + oneMinusAlpha * velocity;
+        lastPos = transform.position;
+        //Debug.Log("Velocity: " + getVelocity().ToString() + "Position: " + getPosition().ToString());
+    }
+
+    public override Vector3 getVelocity()
+    {
+        return resultant;
+    }
+
+    public override Vector3 getPosition()
+    {
+        return transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {}
+}
