7e8011deb3a6e39fcb775845f05adf9e66a87691
Kyle Huang <kylehuang06@gmail.com>
2024-03-07 00:10:59 -0800

    Main Menu Work

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,20 +1,77 @@
-using JetBrains.Annotations;
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class Menu : MonoBehaviour
-{ 
-    public void toLesson1_1()
+{
+
+    public VisualTreeAsset mainMenu;
+    public bool mainMenuInstantiated = false;
+    
+    public VisualTreeAsset lessonsMenu;
+    public bool lessonsMenuInstantiated = false;
+
+    public VisualTreeAsset aboutMenu;
+    public bool aboutMenuInstantiated = false;
+
+    private UIDocument document;
+
+    public void OnEnable()
     {
-        SceneManager.LoadScene("Lesson 1.1", LoadSceneMode.Single);
+        document = GetComponent<UIDocument>();
+        InstantiateMainMenu();
     }
 
-    public void toLesson1_2()
+    public void InstantiateMainMenu()
     {
-        SceneManager.LoadScene("Lesson 1.2", LoadSceneMode.Single);
+        document.visualTreeAsset = mainMenu;
+
+        if (mainMenuInstantiated) { return; }
+        
+        Button lessonsButton = document.rootVisualElement.Q<Button>("LessonsButton");
+        lessonsButton.RegisterCallback<ClickEvent>((_)=> InstantiateLessonsMenu());
+
+        Button settingsButton = document.rootVisualElement.Q<Button>("SettingsButton");
+        settingsButton.RegisterCallback<ClickEvent>((_) => { }); // TODO: Add settings menu
+
+        Button aboutButton = document.rootVisualElement.Q<Button>("AboutButton");
+        aboutButton.RegisterCallback<ClickEvent>((_) => InstantiateAboutMenu());
+
+        mainMenuInstantiated = true;
     }
 
+    public void InstantiateLessonsMenu()
+    {
+        document.visualTreeAsset = lessonsMenu;
 
+        if (lessonsMenuInstantiated) { return; }
+        
+        Button backButton = document.rootVisualElement.Q<Button>("ExitButton");
+        backButton.RegisterCallback<ClickEvent>((_) => InstantiateMainMenu());
+
+        Button lesson1_1 = document.rootVisualElement.Q<Button>("1_1_Button");
+        lesson1_1.RegisterCallback<ClickEvent>((_) => loadScene("Lesson 1.1"));
+
+        Button lesson1_2 = document.rootVisualElement.Q<Button>("1_2_Button");
+        lesson1_2.RegisterCallback<ClickEvent>((_) => loadScene("Lesson 1.2"));
+
+        lessonsMenuInstantiated = true;
+    }
+
+    public void InstantiateAboutMenu()
+    {
+        document.visualTreeAsset = aboutMenu;
+
+        if (aboutMenuInstantiated) { return; }
+
+        Button backButton = document.rootVisualElement.Q<Button>("ExitButton");
+        backButton.RegisterCallback<ClickEvent>((_) => InstantiateMainMenu());
+
+        aboutMenuInstantiated = true;
+    }
+
+    public void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
 }
