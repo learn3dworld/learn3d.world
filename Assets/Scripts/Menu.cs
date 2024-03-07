7adcfc57f592fc64c4b0d1dc2e2321e5fbfdebc8
Kyle Huang <kylehuang06@gmail.com>
2024-03-07 00:15:41 -0800

    Menu Works

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,14 +6,8 @@ public class Menu : MonoBehaviour
 {
 
     public VisualTreeAsset mainMenu;
-    public bool mainMenuInstantiated = false;
-    
     public VisualTreeAsset lessonsMenu;
-    public bool lessonsMenuInstantiated = false;
-
     public VisualTreeAsset aboutMenu;
-    public bool aboutMenuInstantiated = false;
-
     private UIDocument document;
 
     public void OnEnable()
@@ -26,8 +20,6 @@ public class Menu : MonoBehaviour
     {
         document.visualTreeAsset = mainMenu;
 
-        if (mainMenuInstantiated) { return; }
-        
         Button lessonsButton = document.rootVisualElement.Q<Button>("LessonsButton");
         lessonsButton.RegisterCallback<ClickEvent>((_)=> InstantiateLessonsMenu());
 
@@ -36,16 +28,12 @@ public class Menu : MonoBehaviour
 
         Button aboutButton = document.rootVisualElement.Q<Button>("AboutButton");
         aboutButton.RegisterCallback<ClickEvent>((_) => InstantiateAboutMenu());
-
-        mainMenuInstantiated = true;
     }
 
     public void InstantiateLessonsMenu()
     {
         document.visualTreeAsset = lessonsMenu;
 
-        if (lessonsMenuInstantiated) { return; }
-        
         Button backButton = document.rootVisualElement.Q<Button>("ExitButton");
         backButton.RegisterCallback<ClickEvent>((_) => InstantiateMainMenu());
 
@@ -54,20 +42,14 @@ public class Menu : MonoBehaviour
 
         Button lesson1_2 = document.rootVisualElement.Q<Button>("1_2_Button");
         lesson1_2.RegisterCallback<ClickEvent>((_) => loadScene("Lesson 1.2"));
-
-        lessonsMenuInstantiated = true;
     }
 
     public void InstantiateAboutMenu()
     {
         document.visualTreeAsset = aboutMenu;
 
-        if (aboutMenuInstantiated) { return; }
-
         Button backButton = document.rootVisualElement.Q<Button>("ExitButton");
         backButton.RegisterCallback<ClickEvent>((_) => InstantiateMainMenu());
-
-        aboutMenuInstantiated = true;
     }
 
     public void loadScene(string scene)
