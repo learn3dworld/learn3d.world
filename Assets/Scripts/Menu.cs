8389a93f3d7138738a05470ff92c33165aeb87ae
Justin Kim <66839733+belugabcool@users.noreply.github.com>
2024-03-06 22:17:35 -0800

    Menu Image Update

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
@@ -13,6 +11,6 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if()
     }
 }
