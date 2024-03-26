a8ca472588295f7af9e40fa4a940972b3b33c9f3
Justin Kim <belugabcool@gmail.com>
2024-03-26 08:45:58 -0700

    made quiz

---
+++ Assets/Scripts/QuizText.cs
@@ -0,0 +1,86 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class QuizText : MonoBehaviour
+{
+    [SerializeField]
+    private Button[] buttons = new Button[4];
+    [SerializeField]
+    private TextMeshProUGUI questionTextElement;
+    [SerializeField]
+    private string[] answers = new string[4] { "", "", "", "" };
+    [SerializeField]
+    private string questionText = "";
+
+    private string[] questionNames = new string[4]
+    {
+        "A", "B", "C", "D",
+    };
+
+
+    public enum QuestionButton
+    {
+        A1, A2, A3, A4
+    }
+
+    [SerializeField]
+    private QuestionButton correctAnswer;
+
+    public void OnButtonClick(QuestionButton questionButton)
+    {
+        if (correctAnswer == questionButton)
+        {
+            Debug.Log("Correct!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+
+
+    public void OnEnable()
+    {
+        clearListeners();
+
+        buttons[0].onClick.AddListener(() => {
+            OnButtonClick(QuestionButton.A1);
+        });
+
+        buttons[1].onClick.AddListener(() => {
+            OnButtonClick(QuestionButton.A2);
+        });
+
+        buttons[2].onClick.AddListener(() => {
+            OnButtonClick(QuestionButton.A3);
+        });
+
+        buttons[3].onClick.AddListener(() => {
+            OnButtonClick(QuestionButton.A4);
+        });
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+
+            buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = questionNames[i] + ". " + answers[i];
+        }
+
+        questionTextElement.text = "Question: " + questionText;
+    }
+
+    private void clearListeners()
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.RemoveAllListeners();
+            button.enabled = true;
+            button.interactable = true;
+            button.gameObject.SetActive(true);
+        }
+    }
+
+    public void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
